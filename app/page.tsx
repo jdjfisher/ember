e99e636ec2aaa9c52dd7cfacfdbda68bbd5e25c9
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2023-05-11 23:42:13 +0100

    Created placeholder page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-5">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, unde. Beatae ipsam, illo voluptas quisquam modi
-      iusto numquam quia doloremque animi. Ipsam saepe fuga, omnis nisi inventore expedita quis laudantium!
+    <main className="flex flex-col min-h-screen p-5 gap-5">
+      <section className="flex justify-between items-center">
+        <div className="space-y-1">
+          <div className="w-7 h-1 bg-purple-100"></div>
+          <div className="w-7 h-1 bg-purple-100"></div>
+          <div className="w-7 h-1 bg-purple-100"></div>
+        </div>
+        <h1 className="font-semibold text-purple-900">lorem</h1>
+        <div className="w-7 h-7 bg-purple-300"></div>
+      </section>
+
+      <section className="flex-1 bg-purple-300 hover:animate-pulse items-end flex p-5">
+        <h2 className="text-3xl">
+          Lorem Ipsum
+          <br />
+          99
+        </h2>
+      </section>
+
+      <section className="flex justify-around">
+        <div className="w-7 h-7 bg-purple-100"></div>
+        <div className="w-7 h-7 bg-purple-500 shadow-xl"></div>
+        <div className="w-7 h-7 bg-purple-100"></div>
+        <div className="w-7 h-7 bg-purple-100"></div>
+      </section>
     </main>
   );
 }
