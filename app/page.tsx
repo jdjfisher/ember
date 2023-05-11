4ce3902acd5ad0e24722ddd1fbb1972a2b108588
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2023-05-11 23:47:05 +0100

    Install prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen p-5 gap-5">
-      <section className="flex justify-between items-center">
+    <main className="flex min-h-screen flex-col gap-5 p-5">
+      <section className="flex items-center justify-between">
         <div className="space-y-1">
-          <div className="w-7 h-1 bg-purple-100"></div>
-          <div className="w-7 h-1 bg-purple-100"></div>
-          <div className="w-7 h-1 bg-purple-100"></div>
+          <div className="h-1 w-7 bg-purple-100"></div>
+          <div className="h-1 w-7 bg-purple-100"></div>
+          <div className="h-1 w-7 bg-purple-100"></div>
         </div>
         <h1 className="font-semibold text-purple-900">lorem</h1>
-        <div className="w-7 h-7 bg-purple-300"></div>
+        <div className="h-7 w-7 bg-purple-300"></div>
       </section>
 
-      <section className="flex-1 bg-purple-300 hover:animate-pulse items-end flex p-5">
+      <section className="flex flex-1 items-end bg-purple-300 p-5 hover:animate-pulse">
         <h2 className="text-3xl">
           Lorem Ipsum
           <br />
@@ -20,10 +20,10 @@ export default function Home() {
       </section>
 
       <section className="flex justify-around">
-        <div className="w-7 h-7 bg-purple-100"></div>
-        <div className="w-7 h-7 bg-purple-500 shadow-xl"></div>
-        <div className="w-7 h-7 bg-purple-100"></div>
-        <div className="w-7 h-7 bg-purple-100"></div>
+        <div className="h-7 w-7 bg-purple-100"></div>
+        <div className="h-7 w-7 bg-purple-500 shadow-xl"></div>
+        <div className="h-7 w-7 bg-purple-100"></div>
+        <div className="h-7 w-7 bg-purple-100"></div>
       </section>
     </main>
   );
