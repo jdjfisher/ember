2d6b2704b819b34f479623c6e01741230e10e40d
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2024-06-16 09:44:34 +0100

    Restore margin

--- app/(tabs)/page.tsx
+++ app/(tabs)/page.tsx
@@ -5,7 +5,7 @@ import { profiles } from '@/lib/dummy';
 
 export default function Home() {
   return (
-    <div className="relative w-full rounded-3xl bg-[#EE1F4D]">
+    <div className="relative mx-4 my-2 w-full rounded-3xl bg-[#EE1F4D]">
       {profiles.map((profile) => (
         <SwipeCard profile={profile} key={profile.name} />
       ))}
