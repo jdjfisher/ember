7bd5004443229a7f9151f6499dbde9140e915277
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2024-09-10 21:16:27 +0100

    Added refresh

--- app/(tabs)/layout.tsx
+++ app/(tabs)/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import NavTabs from '@/components/NavTabs';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -7,11 +9,17 @@ interface Props {
 }
 
 export default function TabLayout({ children }: Props) {
+  function refresh() {
+    window.location.reload();
+  }
+
   return (
     <>
       <section className="grid grid-cols-3 items-center p-3">
         <h1 className="select-none text-center col-start-2 text-lg font-bold text-[#EE1F4D] items-start">
-          Ember
+          <button type="button" onClick={refresh}>
+            Ember
+          </button>
         </h1>
 
         <Link href="settings" className=" ml-auto">
