7e5598693289eed26c377fc2e3a0ac829fec23c5
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2024-09-10 20:48:11 +0100

    Fixed width

--- app/(tabs)/profile/page.tsx
+++ app/(tabs)/profile/page.tsx
@@ -5,7 +5,7 @@ import { ProfileDetail } from '@/components/ProfileDetail';
 
 export default function Profile() {
   return (
-    <div className="p-5">
+    <div className="p-5 w-full">
       <ProfileDetail profile={hugo} />
     </div>
   );
