0b8bc19f8606e81be9062146d1b2121842725f54
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2024-09-10 21:22:06 +0100

    Unique key

--- app/(tabs)/page.tsx
+++ app/(tabs)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="relative mx-4 my-2 w-full rounded-3xl bg-[#EE1F4D]">
       {profilesToSwipe.map((profile) => (
-        <SwipeCard profile={profile} key={profile.name} />
+        <SwipeCard profile={profile} key={profile.id} />
       ))}
     </div>
   );
