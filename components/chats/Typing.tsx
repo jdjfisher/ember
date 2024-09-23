cf3a4ac4a89116c8d89261aca395259164306669
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2024-09-23 13:23:37 +0100

    Indent typing

--- components/chats/Typing.tsx
+++ components/chats/Typing.tsx
@@ -1,6 +1,6 @@
 export default function Typing() {
   return (
-    <div className="flex w-min items-center gap-1 rounded-lg bg-gray-200 p-2">
+    <div className="flex w-min items-center gap-1 rounded-lg bg-gray-200 p-2 ml-10 select-none">
       <div
         className="h-1 w-1 animate-bounce rounded-full bg-black"
         style={{ animationDelay: '0ms' }}
