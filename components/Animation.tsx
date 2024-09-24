b19132804c7dae34eb5223cdaa0664cd319b09e5
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2024-09-24 10:48:03 +0100

    Use ref in animation component rather than static id

--- components/Animation.tsx
+++ components/Animation.tsx
@@ -1,15 +1,19 @@
-import { useEffect } from 'react';
+'use client';
+
+import { useEffect, useRef } from 'react';
 import lottie from 'lottie-web';
 
 interface Props {
-  id: 'opening' | 'match' | 'unmatch';
+  id: 'opening' | 'match' | 'unmatched';
   onEnd?: () => void;
 }
 
 export default function Animation({ id, onEnd }: Props) {
+  const playerRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     const animation = lottie.loadAnimation({
-      container: document.getElementById('player')!,
+      container: playerRef.current!,
       renderer: 'svg',
       autoplay: true,
       loop: false,
@@ -20,10 +24,12 @@ export default function Animation({ id, onEnd }: Props) {
       animation.addEventListener('complete', onEnd);
     }
 
+    animation.addEventListener('error', console.error);
+
     return () => {
       animation.destroy();
     };
   }, [id, onEnd]);
 
-  return <div id="player"> </div>;
+  return <div ref={playerRef}> </div>;
 }
